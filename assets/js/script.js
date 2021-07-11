0e245c9fb09a3a5fc822fe96f28f4e364505a2c6
JP Mamuric <mamuricj@gmail.com>
2021-07-11 10:49:01 -0500

    working add task button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,11 @@
-console.dir(window.document);
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var btn = window.document.querySelector("button").textContent;
-console.dir(btn);
-
-document.querySelector("#save-task");
-
-console.log(buttonEl);
-
-buttonEl.addEventListener("click", function() {
-    alert("button clicked");
-});
-
-var buttonEl = document.querySelector("#save-task"); 
-var tasksToDoEl = document.querySelector("#tasks-to-do"); 
-
-var createTaskHandler = function() { 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
 
 buttonEl.addEventListener("click", createTaskHandler);
